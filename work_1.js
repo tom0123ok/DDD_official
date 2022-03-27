06b72331053bdd79144f8c48c51af9537794c236
Fukuyama tomoki <80202990+tomoki-yuzuman@users.noreply.github.com>
2022-03-27 08:38:44 +0800

    Update work_1.js

--- work_1.js
+++ work_1.js
@@ -25,3 +25,15 @@ $(window).on('load',function(){
   $("#splash").delay(1500).fadeOut('slow');//ローディング画面を1.5秒（1500ms）待機してからフェードアウト
   $("#splash_logo").delay(1200).fadeOut('slow');//ロゴを1.2秒（1200ms）待機してからフェードアウト
 });
+
+$(".openbtn").click(function () {//ボタンがクリックされたら
+	$(this).toggleClass('active');//ボタン自身に activeクラスを付与し
+    $("#g-nav").toggleClass('panelactive');//ナビゲーションにpanelactiveクラスを付与
+    $("#container").toggleClass('mainblur');//ぼかしたいエリアにmainblurクラスを付与
+});
+
+$("#g-nav a").click(function () {//ナビゲーションのリンクがクリックされたら
+    $(".openbtn").removeClass('active');//ボタンの activeクラスを除去し
+    $("#g-nav").removeClass('panelactive');//ナビゲーションのpanelactiveクラスを除去し
+    $("#container").removeClass('mainblur');//ぼかしているエリアのmainblurクラスを除去
+});
